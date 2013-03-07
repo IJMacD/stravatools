8c047937b7c0dd0f9e1dd0ac09606708f0db1fed
Iain MacDonald <IJMacD@gmail.com>
2013-03-07 00:31:50 +0000

    Now works thanks to proxy, damn Cross Origin Requests and lack of JSONP support

--- js/strava.js
+++ js/strava.js
@@ -2,6 +2,7 @@ $(function(){
   var output = $('#output'),
       progress = $('progress'),
       athlete = 76426,
+      proxyURL = 'proxy.php?url=',
       ridesIndexURL = 'http://www.strava.com/api/v1/rides?athleteId=',
       ridesShowURL = 'http://www.strava.com/api/v1/rides/';
 
@@ -11,27 +12,32 @@ $(function(){
 
   function loadRides(){
     var rides = [];
-    $.get(ridesIndexURL + athlete, function(data){
-      var l = data.rides.length, i=0;
-      progress.attr('max', l);
-      for(;i<l;i++){
-        $.get(ridesShowURL + data.rides[i].id, function(data){
-          rides.push(data.ride);
-          progress.val(rides.length);
-          if(rides.length == l){
-            displayRides(rides);
-          }
-        });
+    $.getJSON(proxyURL + ridesIndexURL + athlete,
+      function(data){
+        var l = data.rides.length, i=0;
+        progress.attr('max', l);
+        for(;i<l;i++){
+          $.getJSON(proxyURL + ridesShowURL + data.rides[i].id,
+            function(data){
+              rides.push(data.ride);
+              progress.val(rides.length);
+              if(rides.length == l){
+                displayRides(rides);
+                progress.val(0);
+              }
+            }
+          );
+        }
       }
-    });
+    );
   }
 
   function displayRides(rides){
-    var i=0; l=rides.length, ride,
+    var i=0, l=rides.length, ride,
         out = ["Name, Start, Duration, Distance"];
     for(;i<l;i++){
       ride = rides[i];
-      out.push(ride.name + ", " + ride.startDate + ", " + ride.elapsedTime + ", " + ride.distance);
+      out.push(ride.name.replace(/,/g, '') + ", " + (new Date(ride.startDate)).toLocaleString() + ", " + ride.elapsedTime + ", " + ride.distance);
     }
     output.html(out.join("<br>\n"));
   }
